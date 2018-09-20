93f87ecc85d474bd1818064e1130003765e1fdbc
Sergey <sergey.batalev@yandex.ru>
2018-09-20 22:22:58 +0300

    user middleware

--- app/routes/userHandlers.go
+++ app/routes/userHandlers.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/likipiki/RewriteNotes/app/postgres"
 )
@@ -14,4 +17,21 @@ func (handlers UserHandlers) Router() *chi.Mux {
 	r := chi.NewRouter()
 
 	return r
+}
+
+func (handlers UserHandlers) NoteCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID := chi.URLParam(r, "id")
+		if userID == "" {
+			next.ServeHTTP(w, r)
+			return
+		}
+		user, err := handlers.Controller.Get(userID)
+		if err != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user", user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
